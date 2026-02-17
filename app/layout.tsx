adf94d46e75509cf14080f7784e3fed1a3e2ecb3
raihanrahman90 <raihanr090@gmail.com>
2026-02-17 22:49:29 +0800

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="/logo.png" />
-      </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
